e10ca5ee15e78951111f7468bee6ea0450c366a5
Layton-Buckley <layton.buckley5@gmail.com>
2025-04-27 15:56:44 -0600

    Update sketch2.js

--- finalProject/sketch2.js
+++ finalProject/sketch2.js
@@ -19,13 +19,14 @@ function mousePressed() {
     guitarLeft.play();
     background(0, 255, 0);
   }
-}
-
-function playMusic(){
   const Audio1 = new Audio(guitarLeft);
   const Audio2 = new Audio(drums);
 
   Audio1.addEventListener("play", function(){
     Audio2.play();
   });
+}
+
+function playMusic(){
+
 }
