fd11d7e460a2ad56e8651743ba58ec1ddfe23d34
Layton-Buckley <layton.buckley5@gmail.com>
2025-04-28 14:00:54 -0600

    Update sketch2.js

--- finalProject/sketch2.js
+++ finalProject/sketch2.js
@@ -3,11 +3,11 @@ let sounds = [];
 let playButton;
 
 function preload() {
-  sounds.push(loadSound('./musicSources/bass2.mp3'));
-  sounds.push(loadSound('./musicSources/drums2.mp3'));
-  sounds.push(loadSound('./musicSources/guitarLeft2.mp3'));
-  sounds.push(loadSound('./musicSources/guitarRight2.mp3'));
-  sounds.push(loadSound('./musicSources/vocals2.mp3'));
+  sounds.push(loadSound('./musicSources/bass2.mp3', () => console.log('bass2 loaded')));
+  sounds.push(loadSound('./musicSources/drums2.mp3', () => console.log('drums2 loaded')));
+  sounds.push(loadSound('./musicSources/guitarLeft2.mp3', () => console.log('guitarLeft2 loaded')));
+  sounds.push(loadSound('./musicSources/guitarRight2.mp3', () => console.log('guitarRight2 loaded')));
+  sounds.push(loadSound('./musicSources/vocals2.mp3', () => console.log('vocals2 loaded')));
 }
 
 function setup() {
