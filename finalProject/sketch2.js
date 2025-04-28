ae2b1f0aedf8a2d15e713a51174287278d43b5aa
Layton-Buckley <layton.buckley5@gmail.com>
2025-04-28 13:33:33 -0600

    fix?

--- finalProject/sketch2.js
+++ finalProject/sketch2.js
@@ -1,12 +1,29 @@
 
+let sounds = [];
+let playButton;
 
-function setup() {
-
-  createCanvas(720, 200);
-  background(255, 0, 0);
+function preload() {
+  sounds.push(loadSound('musicSources/bass.mp3'));
+  sounds.push(loadSound('musicSources/drums.mp3'));
+  sounds.push(loadSound('musicSources/guitarLeft.mp3'));
+  sounds.push(loadSound('musicSources/guitarRight.mp3'));
+  sounds.push(loadSound('musicSources/vocals.mp3'));
 }
 
+function setup() {
+  createCanvas(400, 200);
 
+  // Create a play button
+  playButton = createButton('Play All Sounds');
+  playButton.position(20, 20);
+  playButton.mousePressed(playAllSounds);
+}
 
-
-
+function playAllSounds() {
+  // Play all sounds if they are loaded
+  for (let s of sounds) {
+    if (s.isLoaded()) {
+      s.play();
+    }
+  }
+}
