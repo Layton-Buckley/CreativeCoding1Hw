70c0fbc5b8e48b11319d66cb2b4f7366b5cbf48f
Layton-Buckley <layton.buckley5@gmail.com>
2025-04-28 14:33:20 -0600

    Update sketch2.js

--- finalProject/sketch2.js
+++ finalProject/sketch2.js
@@ -4,7 +4,6 @@ let playButton;
 
 function preload() {
   sounds.push(loadSound('./musicSources/bass2.mp3', () => console.log('bass2 loaded')));
-
 }
 
 function setup() {
@@ -13,12 +12,14 @@ function setup() {
   // Create a play button
   playButton = createButton('Play');
   playButton.position(20, 20);
-  playButton.mousePressed(playAllSounds);
+  
+  playButton.mousePressed(() => {
+    userStartAudio(); // FIRST, resume audio context
+    playAllSounds();  // THEN play
+  });
 }
 
 function playAllSounds() {
-  // Play all sounds if they are loaded
-  userStartAudio();
   for (let s of sounds) {
     if (s.isLoaded()) {
       s.play();
